f0359f64df54b9e4eb9609af901c53c33e0794ff
DongHyu <ehdgb97@gmail.com>
2025-03-16 20:49:38 +0900

    Initial commit

---
+++ Source/RabbitProject/Public/RoseAnim.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "RoseAnim.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RABBITPROJECT_API URoseAnim : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
